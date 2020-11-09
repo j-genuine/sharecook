1322aa142ee4ecff391d700519707bba5b5f933f
j.genuine <devjnsq@gmail.com>
2020-11-09 06:16:26 +0000

    prepare for heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(WorkersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(AreasTableSeeder::class);
+        $this->call(SkillsTableSeeder::class);
     }
 }
--- database/seeds/WorkersTableSeeder.php
+++ database/seeds/WorkersTableSeeder.php
@@ -11,33 +11,22 @@ class WorkersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('workers')->insert([
-            'name' => 'テスト料理人１',
-            'email' => 'test1@jnsq.net',
-            'password' => bcrypt('11112222'),
-            'public_flag' => 1, 
-            'phone' => '03011112222', 
-            'nickname' => 'シェフ１', 
-            'price_lunch' => 2000, 
-            'price_dinner' => 3000, 
-            'amature_career' => 2, 
-            'pro_career' => 3, 
-            'portrait_filename' => '', 
-            'comment' => 'パスタが得意です。', 
-        ]);
-        DB::table('workers')->insert([
-            'name' => 'テスト料理人２',
-            'email' => 'test2@jnsq.net',
-            'password' => bcrypt('11112222'),
-            'public_flag' => 1, 
-            'phone' => '03022223333', 
-            'nickname' => 'シェフ２', 
-            //'price_lunch' => 0,
-            'price_dinner' => 4000, 
-            'amature_career' => 5, 
-            'pro_career' => 10, 
-            'portrait_filename' => '', 
-            'comment' => '料亭で10年間修業しました。', 
-        ]);
+        for ($i = 1; $i <= 10; $i++) {
+        
+            DB::table('workers')->insert([
+                'name' => 'テスト料理人'.$i,
+                'email' => 'test'.$i.'@jnsq.net',
+                'password' => bcrypt('11112222'),
+                'public_flag' => 1, 
+                'phone' => '09011112222', 
+                'nickname' => 'シェフ'.$i, 
+                'price_lunch' => 2000, 
+                'price_dinner' => 3000, 
+                'amature_career' => 2, 
+                'pro_career' => 3, 
+                'portrait_filename' => '', 
+                'comment' => 'テストコメント', 
+            ]);
+        }
     }
 }
